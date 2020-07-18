aa12a6f281c29da393b4ed2309587de6dadcc66e
brynrac <brynrace@gmail.com>
2020-07-17 21:17:24 -0700

    Fix map resizing issue

--- src/components/content/Map.js
+++ src/components/content/Map.js
@@ -4,47 +4,48 @@ import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import Spinner from '../Spinner';
 import PopUp from './PopUp';
 
-function debounce(fn, ms) {
-  let timer;
-  return (_) => {
-    clearTimeout(timer);
-    timer = setTimeout((_) => {
-      timer = null;
-      fn.apply(this, arguments);
-    }, ms);
-  };
-}
+// function debounce(fn, ms) {
+//   let timer;
+//   return (_) => {
+//     clearTimeout(timer);
+//     timer = setTimeout((_) => {
+//       timer = null;
+//       fn.apply(this, arguments);
+//     }, ms);
+//   };
+// }
 
 export default function Map({ covid }) {
-  const [dimensions, setDimensions] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+  // const [dimensions, setDimensions] = useState({
+  //   width: '100vw',
+  //   height: '100vh',
+  // });
 
   const [viewport, setViewport] = useState({
     lat: 39.8283,
     lng: 98.5795,
     zoom: 2,
-    width: '100vw',
-    height: '100vh',
   });
   const [activeMarker, setActiveMarker] = useState(null);
 
   useEffect(() => {
-    const debouncedHandleResize = debounce(function handleResize() {
-      setViewport({ ...viewport }, { width: '100vw', height: '100vh' });
-    }, 1000);
+    // const debouncedHandleResize = debounce(function handleResize() {
+    //   setViewport(
+    //     { ...viewport },
+    //     { width: dimensions.width, height: dimensions.height }
+    //   );
+    // }, 1000);
 
     const listener = (e) => {
       if (e.key === 'Escape') {
         setActiveMarker(null);
       }
     };
-    window.addEventListener('resize', debouncedHandleResize);
+    // window.addEventListener('resize', debouncedHandleResize);
     window.addEventListener('keydown', listener);
     return (_) => {
       window.removeEventListener('keydown', listener);
-      window.removeEventListener('resize', debouncedHandleResize);
+      // window.removeEventListener('resize', debouncedHandleResize);
     };
   }, []);
 
@@ -60,6 +61,8 @@ export default function Map({ covid }) {
           mapboxApiAccessToken={token}
           mapStyle="mapbox://styles/gaiza/ckco24rjf35zq1irwzq6dl5td"
           onViewportChange={(viewport) => setViewport(viewport)}
+          width={'100vw'}
+          height={'100vh'}
         >
           {covid.covidData.map((country) => (
             <Marker
