4d0dab400f1d217516feee7832022f6c5fd82487
brynrac <brynrace@gmail.com>
2020-07-19 20:59:58 -0700

    Add news story info and initial CSS

--- src/components/content/NewsItem.js
+++ src/components/content/NewsItem.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-export default function NewsItem({story}) {
-    return (
-        <li className="news-item">
-            News-item
-        </li>
-    )
+// ampWebUrl: "https://www.washingtonpost.com/nation/2020/07/19/coronavirus-update-us/?outputType=amp"
+// categories: ["news"]
+// cdnAmpWebUrl: "https://www-washingtonpost-com.cdn.ampproject.org/c/s/www.washingtonpost.com/nation/2020/07/19/coronavirus-update-us/?outputType=amp"
+// excerpt: "Coronavirus infections rose in states from every region of the country over the past week, with more than a dozen states on Saturday reaching record highs in their seven-day averages for new daily cases."
+// heat: 102
+// images: [{…}]
+// locale: "en-us"
+// path: "_news/2020-07-19-coronavirus-updates-trump-dismisses-rising-coronavirus-cases-as-deaths-mount.md"
+// provider: {name: "Washington Post", domain: "washingtonpost.com", images: null, publishers: null, authors: null}
+// publishedDateTime: "2020-07-19T06:38:00-07:00"
+// tags: ["US"]
+// title: "Coronavirus updates: Trump dismisses rising coronavirus cases as deaths mount"
+// topics: (2) ["Coronavirus in US", "Coronavirus"]
+// type: "article"
+// updatedDateTime: null
+
+export default function NewsItem({ story }) {
+  return (
+    <li className="news-item">
+      <div>
+        <h3>{story.title}</h3>
+        <p className="news-item-date">
+          {story.publishedDateTime.split('T')[0]}
+        </p>
+      </div>
+    </li>
+  );
 }
