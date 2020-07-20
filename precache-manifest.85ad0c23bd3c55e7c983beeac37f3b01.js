c16c4cedf07f733b751e681c08fcd35a358a53db
brynrac <brynrace@gmail.com>
2020-07-20 16:07:08 -0700

    Updates

---
+++ precache-manifest.85ad0c23bd3c55e7c983beeac37f3b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13eda2705e22c578e003ed4c0d2b1358",
+    "url": "/cv19tracker/index.html"
+  },
+  {
+    "revision": "6953a3d9434f8b3cadc6",
+    "url": "/cv19tracker/static/css/main.af548dac.chunk.css"
+  },
+  {
+    "revision": "6490373587e36a6c34a0",
+    "url": "/cv19tracker/static/js/2.4f9b047f.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/cv19tracker/static/js/2.4f9b047f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6953a3d9434f8b3cadc6",
+    "url": "/cv19tracker/static/js/main.9843d5cd.chunk.js"
+  },
+  {
+    "revision": "5b2ad438133b067a5626",
+    "url": "/cv19tracker/static/js/runtime-main.ba2e4d7b.js"
+  }
+]);
